e391865de2d18a9adaab3bfb014d32923289b0e4
Mikas60 <Mikas60man@gmail.com>
2016-10-14 09:33:34 +0100

    Added player 2 controls

--- Program.cs
+++ Program.cs
@@ -19,28 +19,51 @@ namespace ConsoleApplication
             while(true)
             {
 
-                string direction = "";
+                if(Console.KeyAvailable)
+                {
+                    string direction = "";
+                    string direction2 = "";
 
-                ConsoleKeyInfo keyPress = Console.ReadKey(true);
-                
-                if(keyPress.Key == ConsoleKey.UpArrow)
-                {
-                    direction = "up";
-                }
-                else if(keyPress.Key == ConsoleKey.DownArrow)
-                {
-                    direction = "down";
-                } 
-                else if(keyPress.Key == ConsoleKey.LeftArrow)
-                {
-                    direction = "left";
-                }
-                else if(keyPress.Key == ConsoleKey.RightArrow)
-                {
-                    direction = "right";
+                    ConsoleKeyInfo keyPress = Console.ReadKey(true);
+                    
+                    if(keyPress.Key == ConsoleKey.UpArrow)
+                    {
+                        direction = "up";
+                    }
+                    else if(keyPress.Key == ConsoleKey.DownArrow)
+                    {
+                        direction = "down";
+                    } 
+                    else if(keyPress.Key == ConsoleKey.LeftArrow)
+                    {
+                        direction = "left";
+                    }
+                    else if(keyPress.Key == ConsoleKey.RightArrow)
+                    {
+                        direction = "right";
+                    }
+                    else if(keyPress.Key == ConsoleKey.W)
+                    {
+                        direction2 = "up";
+                    }
+                    else if(keyPress.Key == ConsoleKey.S)
+                    {
+                        direction2 = "down";
+                    } 
+                    else if(keyPress.Key == ConsoleKey.A)
+                    {
+                        direction2 = "left";
+                    }
+                    else if(keyPress.Key == ConsoleKey.D)
+                    {
+                        direction2 = "right";
+                    }
+
+                    player1.Move(direction);
+                    player2.Move(direction2);
+
                 }
 
-                player1.Move(direction);
                 Console.Clear();
                 player1.Draw();
                 player2.Draw();
