e297c6b12da03021fb019c77015d385eec67bf78
Mikas60 <FN229@student.anglia.ac.uk>
2016-10-21 10:09:49 +0100

    added items, created inventory, adde maxItems,

--- Character.cs
+++ Character.cs
@@ -9,6 +9,7 @@ namespace ConsoleApplication
         {
 
             
+            
         }
 
     }
--- Inventory.cs
+++ Inventory.cs
@@ -1,14 +1,82 @@
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
     public class Inventory
     {
 
-        public Inventory()
+        int maxItems = 8;
+
+        public int MaxItems
         {
 
+            get {return maxItems;}
+            set {maxItems = value;}
+
+        }
+        public List<Item> Items = new List<Item>();
+
+        public bool IsOpen {get; set;}
+
+        public Inventory()
+        {
             
+        }
+
+        public void AddItem(Item item)
+        {
+            if(IsOpen)
+            {
+                if(Items.Count < maxItems)
+                {
+                    Items.Add(item);
+                }
+                else
+                {
+                    Console.WriteLine("Inventory is full!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Inventory is closed!");
+            }
+        }
+
+
+        public void Open()
+        {
+            if(IsOpen)
+            {
+                Console.WriteLine("Your Inventory is already open.");
+            }
+            else
+            {
+                Console.WriteLine("Your Inventory is now open.");
+                IsOpen = true;
+            }
+        }
+
+        public void Close()
+        {
+            if(IsOpen)
+            {
+                Console.WriteLine("Your Inventory is now closed.");
+                IsOpen = false;
+            }
+            else
+            {
+                Console.WriteLine("Your Inventory is already close.");
+            }
+        }
+
+        public void ListInventory()
+        {
+
+            foreach(Item item in Items)
+            {
+                item.Describe();
+            }
         }
 
     }
